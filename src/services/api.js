d57349636e4616f987b005e1beda754df44cdee6
GabrielVanzo <gabriel.vanzo98@gmail.com>
2022-11-09 17:13:30 -0300

    conclusao requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,16 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const promise = await fetch(url);
+  const data = await promise.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery($CATEGORY_ID, $QUERY) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${$CATEGORY_ID}&q=${$QUERY}`;
+  const promise = await fetch(url);
+  const data = await promise.json();
+  return data;
 }
 
 export async function getProductById() {
