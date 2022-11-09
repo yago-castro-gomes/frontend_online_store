041dcdf63c390c56b92be1aeec1574ca54bcdf0e
Bruno Nabarrete <brunonabarrete@hotmail.com>
2022-11-09 16:10:11 -0300

    adicionamos e finalizamos o requisito 02

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import InitialPage from './pages/InitialPage';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        commit inicial
+        <InitialPage />
       </div>
     );
   }
