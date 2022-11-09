29701bf942bc7a4c62d82da2fc32df4cefa75351
AndrewDluca <andrews.adlb@gmail.com>
2022-11-09 17:37:30 -0300

    requisito 4

--- src/pages/InitialPage.jsx
+++ src/pages/InitialPage.jsx
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class InitialPage extends Component {
+  state = {
+    categories: [],
+  };
+
+  async componentDidMount() {
+    const categoriesData = await getCategories();
+    this.setState({ categories: categoriesData });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
-        <label htmlFor="searchInput">
-
-          <input type="text" name="" id="searchInput" />
-        </label>
-        <h4 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h4>
+        <ul>
+          {
+            categories.map((categorie) => (
+              <li key={ categorie.id }>
+                {categorie.name}
+                <button
+                  type="button"
+                  data-testid="category"
+                  name={ categorie.id }
+                  id={ categorie.id }
+                  value={ categorie.id }
+                >
+                  a
+                </button>
+              </li>
+            ))
+          }
+        </ul>
+        <div>
+          <label htmlFor="searchInput">
+            <input type="text" name="" id="searchInput" />
+          </label>
+          <h4 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h4>
+        </div>
       </div>
     );
   }
