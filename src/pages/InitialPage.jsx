f2e36a3fc48d1d3b5650a756349eb4818a0aba71
Thiago William De Oliveira Santana <110832190+ThiagoWOS@users.noreply.github.com>
2022-11-09 17:50:48 -0300

    Update InitialPage.jsx
    formatando eslint

--- src/pages/InitialPage.jsx
+++ src/pages/InitialPage.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
 import { Link } from 'react-router-dom';
-
+import { getCategories } from '../services/api';
 
 class InitialPage extends Component {
   state = {
